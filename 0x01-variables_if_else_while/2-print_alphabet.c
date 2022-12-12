937c09051789c7321951e67e0e086837bc60f79c
Elaki1 <elakiwasamuel@gmail.com>
2022-12-12 08:29:56 -0800

    look

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
+	
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 review
 okay
+uniform
 
